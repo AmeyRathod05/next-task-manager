8b87aa844e53915bf43918d5feb205a23d78224f
Amey Rathod <rathodamey429@gmail.com>
2026-03-22 16:34:19 +0530

    Fix PrismaClient import - use generated client path

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../generated/prisma/index'
 
 const prisma = new PrismaClient({
   accelerateUrl: process.env.DATABASE_URL!
